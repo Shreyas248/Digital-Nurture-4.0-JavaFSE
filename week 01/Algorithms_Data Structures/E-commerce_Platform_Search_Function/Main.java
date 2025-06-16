877e55a5cd9609cebb5a90065c8e5d853ebf704f
Shreyas <110676491+Shreyas248@users.noreply.github.com>
2025-06-16 22:15:00 +0530

    Add files via upload

---
+++ week 01/Algorithms_Data Structures/E-commerce_Platform_Search_Function/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Product[] products = {
+            new Product(101, "Laptop", "Electronics"),
+            new Product(203, "Shoes", "Footwear"),
+            new Product(150, "Phone", "Electronics"),
+            new Product(99, "Book", "Stationery")
+        };
+
+        Product result1 = SearchAlgorithms.linearSearch(products, 150);
+        System.out.println("Linear Search Result: " + (result1 != null ? result1 : "Not Found"));
+
+        SearchAlgorithms.sortProductsById(products);
+        
+        Product result2 = SearchAlgorithms.binarySearch(products, 150);
+        System.out.println("Binary Search Result: " + (result2 != null ? result2 : "Not Found"));
+    }
+}
