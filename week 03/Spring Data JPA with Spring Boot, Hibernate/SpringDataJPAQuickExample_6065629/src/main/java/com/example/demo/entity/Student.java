f87b4788249d3601ee1107d244c46b0c8e438d38
Shreyas <110676491+Shreyas248@users.noreply.github.com>
2025-07-04 11:31:14 +0530

    Add files via upload

---
+++ week 03/Spring Data JPA with Spring Boot, Hibernate/SpringDataJPAQuickExample_6065629/src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public Student() {}
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+               "id=" + id +
+               ", name='" + name + '\'' +
+               ", email='" + email + '\'' +
+               '}';
+    }
+}
