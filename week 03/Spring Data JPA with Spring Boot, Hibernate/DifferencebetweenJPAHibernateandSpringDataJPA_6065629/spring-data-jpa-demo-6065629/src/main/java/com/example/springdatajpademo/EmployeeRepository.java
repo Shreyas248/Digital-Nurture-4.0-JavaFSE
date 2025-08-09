44478f59f010463c39b4c73096d76f1abad28223
Shreyas-6065629 <shreyaspremjith@gmail.com>
2025-08-10 01:40:10 +0530

    Added hello.xml in Git-T03-HOL_002 branch

--- week 03/Spring Data JPA with Spring Boot, Hibernate/DifferencebetweenJPAHibernateandSpringDataJPA_6065629/spring-data-jpa-demo-6065629/src/main/java/com/example/springdatajpademo/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springdatajpademo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
